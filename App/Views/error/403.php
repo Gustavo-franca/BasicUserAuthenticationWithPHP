9cb87cde33b5ed60dea760ec8e8350b1a0c5b615
Gustavo Cândido <gustavocandidofranca@gmail.com>
2020-09-11 15:40:32 -0300

    criado layot de footer

---
+++ App/Views/error/403.php
@@ -0,0 +1,8 @@
+<div class="error">
+    <h1>Opa você não tem Permissão para acessar essa página! :(</h1>
+    <h2> Tente fazer o login ou se cadastrar para poder acessar :)</h2>
+    <a href="<?php echo APP_HOST ?>/user/login  ">Login</a>
+    <a href="<?php echo APP_HOST ?>/user">Cadastre-se</a>
+
+
+</div>
