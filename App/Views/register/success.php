cd3cd6c910b119c45d6d73132b793c712df93495
Gustavo Cândido <gustavocandidofranca@gmail.com>
2020-09-11 15:43:30 -0300

    html da página de sucesso

---
+++ App/Views/register/success.php
@@ -0,0 +1,6 @@
+<div class="success">
+    <h1>Parabéns <?php echo $viewVar["fistName"]." ".$viewVar["lastName"]?> Você foi cadastrado com sucesso!</h1>
+
+    <a href="<?php echo APP_HOST ?>user/login">Click Aqui para Entrar</a>
+
+</div>
