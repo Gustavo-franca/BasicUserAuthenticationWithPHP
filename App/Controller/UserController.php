db154e27b232026b87dbd0f611bfc8faadd0c3db
Gustavo Cândido <gustavocandidofranca@gmail.com>
2020-09-11 18:32:36 -0300

    adicionado validação dos campos da requisição

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -6,29 +6,45 @@
 
     use App\Model\User\User;
     use App\Model\User\UserDAO;
+    use App\Lib\UserValidate;
     use Exception;
 
     class UserController extends BaseController {
 
         function create($request){
-            $user = new User();
-            $user->fistName = $request["fistName"];
-            $user->lastName = $request["lastName"];
-            $user->email = $request["email"];
-            $user->password = $request["password"];
-            $user->birthday =date($request["birthday"]);
-            $user->bio = $request["bio"];
+            $userValidate = new UserValidate();
+            try{
+                $userValidate->create($request);
+         
+                $user = new User();
+                $user->fistName = $request["fistName"];
+                $user->lastName = $request["lastName"];
+                $user->email = $request["email"];
+                $user->password = $request["password"];
+                $user->birthday =date($request["birthday"]);
+                $user->bio = $request["bio"];
 
-            $userDao = new UserDAO();
-            if($userDao->save($user)){
-               return  $this->render("/register/success",get_object_vars($user));
+                $userDao = new UserDAO();
+                if($userDao->save($user)){
+                return  $this->render("/register/success",get_object_vars($user));
 
-            }
+                }
 
-           return  $this->render("register/index",get_object_vars($user));
+                return  $this->render("register/index",get_object_vars($user));
+            }catch(\Exception $err){
+                $response = [];
+                $response["message"] = $err->getMessage();
+                $response = array_merge($response,$request);
+                return $this->render("register/index",
+                $response);
+              }
+
         }
         
         function update($request){
+            $userValidate = new UserValidate();
+            try{
+                $userValidate->update($request);
             $user = new User();
           
             $user->id = $request["id"];
@@ -41,12 +57,23 @@
             $userDao->update($user);
 
             var_dump("Seus Dados foram atualizados!");
+        }catch(\Exception $err){
+            $response = [];
+            $response["message"] = $err->getMessage();
+            $response = array_merge($response,$request);
+            return $this->render("register/index",
+            $response);
+          }
+
         }
 
        
 
         public function validate($request){
+            $userValidate = new UserValidate();
             try{
+                $userValidate->login($request);
+                
                 $email = $request["email"];
                 $password = $request["password"];
 
